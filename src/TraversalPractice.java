e55231b40a74a9b24336bdf15198adda666fb9c1
tk421 <42457203+tk421bsod@users.noreply.github.com>
2026-06-03 11:45:58 -0700

    Implement printOddNodesPreOrder

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -50,7 +50,13 @@ public class TraversalPractice {
      * @param node the root of the tree
      */
     public static void printOddNodesPreOrder(TreeNode node) {
-
+        if (node == null) return;
+        
+        if (node.data % 2 != 0) {
+            System.out.println(node.data);
+        }
+        printOddNodesPreOrder(node.left);
+        printOddNodesPreOrder(node.right);
     }
     
     /**
